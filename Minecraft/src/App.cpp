b413857b75a57659f49cd9aa36378b5705d6b7b9
Nick <nickbezhok@gmail.com>
2020-03-04 00:24:03 +0300

    App refactoring

--- Minecraft/src/App.cpp
+++ Minecraft/src/App.cpp
@@ -5,11 +5,12 @@
 #include "App.h"
 #include "block_db.h"
 #include "Menu.h"
-
+#include "MapMeshBuilder.h"
 
 using std::to_string;
 using std::advance;
 using std::make_shared;
+using std::make_unique;
 using std::copy;
 using std::inserter;
 using std::fabs;
@@ -24,100 +25,106 @@ App::App(sf::RenderWindow &window)
     m_text.setCharacterSize(20);
     m_text.setFillColor(sf::Color(0, 0, 0, 200));
 
+    m_map = std::make_shared<World::Map>();
+    m_renderer = std::make_unique<Renderer>();
+    m_map_mesh_builder = std::make_unique<MapMeshBuilder>();
 
-    m_map = make_shared<World::Map>();
     m_window.setMouseCursorVisible(false);
-}
 
-void App::draw_SFML() {
-
-    if (m_player.m_is_under_water) {
-        static sf::RectangleShape underwater_filter;
-        underwater_filter.setSize({static_cast<float>(m_window.getSize().x), static_cast<float>(m_window.getSize().y)});
-        underwater_filter.setFillColor({28, 40, 155, 150});
-        underwater_filter.setPosition(0, 0);
-        m_renderer.draw_SFML(underwater_filter);
-    }
-
-    //draw sfml
-    if (m_should_display_debug_info) {
-        m_text.setString(
-                "fps: " +
-                to_string(int(m_debug_data.get_fps())) + "\n" +
-                "ft: " +
-                to_string(int(m_debug_data.get_frame_time())) + "\n" +
-                "x, y, z: " +
-                to_string(m_player.get_position().x) + " " +
-                to_string(m_player.get_position().y) + " " +
-                to_string(m_player.get_position().z) + "\n" +
-
-                to_string(m_map_mesh_builder.get_chunks4rendering_size()) + " " +
-                to_string(m_map->get_size()) + " " +
-                to_string(m_map_mesh_builder.get_chunks4vertices_generation_size()) + " " +
-                //to_string(m_map->m_global_vao_vbo_buffers.size()) + " " +
-                "- chunks rendering, map size, updates, global buffer\n" +
-
-                to_string(vertices_wasnt_free) + " - chunks which veticies memory is not freed"
-        );
-        m_renderer.draw_SFML(m_text);
-    }
-
-    for (auto &e : m_menu->get_spites()) {
-        m_renderer.draw_SFML(e.second);
-    }
-
-    for (auto &e : m_menu->get_top_spites()) {
-        m_renderer.draw_SFML(e.second);
-    }
-}
-
-void App::draw_openGL() {
-    m_map_mesh_builder.regenerate_edited_chunk_vertices();
-
-    m_map_mesh_builder.add_new_chunks2rendering();
-}
-
-void App::run() {
-    DB db;
-    db.load_blocks();
+    DB::load_blocks();
     // after !!!
     m_player.init(m_map.get());
     m_player.god_on();
     m_player.flight_on();
 
+    m_menu = std::make_unique<Menu>(m_window);
+    m_menu->update_players_blocks(m_player);
+}
 
-    Menu menu(m_window);
-    menu.update_players_blocks(m_player);
-    m_menu = &menu;
+std::string App::debug_text() {
+    std::string str =
+            "fps: " +
+          to_string(int(m_debug_data.get_fps())) + "\n" +
+          "ft: " +
+          to_string(int(m_debug_data.get_frame_time())) + "\n" +
+          "x, y, z: " +
+          to_string(m_player.get_position().x) + " " +
+          to_string(m_player.get_position().y) + " " +
+          to_string(m_player.get_position().z) + "\n" +
+          to_string(m_map_mesh_builder->get_chunks4rendering_size()) + " " +
+          to_string(m_map->get_size()) + " " +
+          to_string(m_map_mesh_builder->get_chunks4vertices_generation_size()) + " " +
+          "- chunks rendering, map size, updates, global buffer\n" +
+          to_string(vertices_wasnt_free) + " - chunks which veticies memory is not freed";
+
+    return str;
+}
+
+void App::draw_under_water_filter() {
+    static sf::RectangleShape underwater_filter;
+    underwater_filter.setSize({static_cast<float>(m_window.getSize().x), static_cast<float>(m_window.getSize().y)});
+    underwater_filter.setFillColor({28, 40, 155, 150});
+    underwater_filter.setPosition(0, 0);
+    m_renderer->draw_SFML(underwater_filter);
+}
+
+void App::draw_SFML() {
+
+    if (m_player.m_is_under_water) {
+        draw_under_water_filter();
+    }
+
+    //draw sfml
+    if (m_should_display_debug_info) {
+        m_text.setString(debug_text());
+        m_renderer->draw_SFML(m_text);
+    }
+
+    for (auto &e : m_menu->get_spites()) {
+        m_renderer->draw_SFML(e.second);
+    }
+
+    for (auto &e : m_menu->get_top_spites()) {
+        m_renderer->draw_SFML(e.second);
+    }
+}
+
+void App::draw_openGL() {
+    m_map_mesh_builder->regenerate_edited_chunk_vertices();
+    m_map_mesh_builder->add_new_chunks2rendering();
+}
+
+void App::run() {
+
 
 //	Sleep(1000.f);
-    m_map_mesh_builder.launch(m_map.get(), &m_player, &m_window);
+    m_map_mesh_builder->launch(m_map.get(), &m_player, &m_window);
 
     while (m_window.isOpen()) {
         m_debug_data.start();
         //
-        m_map_mesh_builder.m_mutex__chunks4rendering.lock();
+        m_map_mesh_builder->m_mutex__chunks4rendering.lock();
         for (auto &e : m_map->m_should_be_freed_buffers) {
             glDeleteVertexArrays(1, &e.VAO);
             glDeleteBuffers(1, &e.VBO);
         }
         m_map->m_should_be_freed_buffers.clear();
-        m_map_mesh_builder.m_mutex__chunks4rendering.unlock();
+        m_map_mesh_builder->m_mutex__chunks4rendering.unlock();
         //
         update();
 
         draw_openGL();
         draw_SFML();
 
-        m_renderer.finish_render(m_window, m_player, m_map_mesh_builder.m_chunks4rendering,
-                                 m_map_mesh_builder.m_mutex__chunks4rendering);
+        m_renderer->finish_render(m_window, m_player, m_map_mesh_builder->m_chunks4rendering,
+                                 m_map_mesh_builder->m_mutex__chunks4rendering);
 
         if (m_should_display_debug_info) {
             m_debug_data.count();
         }
     }
 
-    m_map_mesh_builder.wait();
+    m_map_mesh_builder->wait();
 }
 
 void App::handle_events() {
@@ -160,57 +167,40 @@ void App::input() {
             case sf::Event::Closed:
                 m_window.close();
                 break;
-            case sf::Event::KeyPressed: {
-                switch (event.key.code) {
-                    case sf::Keyboard::Escape:
-                        m_window.setMouseCursorVisible(m_should_fix_cursor);
-                        m_should_fix_cursor = !m_should_fix_cursor;
-
-                        break;
-                    case sf::Keyboard::F3:
-                        m_should_display_debug_info = !m_should_display_debug_info;
-                        break;
-                    default:
-                        break;
-                }
+            case sf::Event::KeyPressed:
+                process_key(event.key.code);
                 break;
-            }
-            case sf::Event::Resized: {
-
-                sf::FloatRect visibleArea(0.f, 0.f, static_cast<float>(event.size.width),
-                                          static_cast<float>(event.size.height));
-                m_window.setView(sf::View(visibleArea));
-                m_window.setSize({event.size.width, event.size.height});
-                m_menu->update();
+            case sf::Event::Resized:
+                resize(event.size.width, event.size.height);
                 break;
-            }
         }
 
 
-        // block input while ctrl pressed
-        static bool input_ability = true;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) ||
-            sf::Keyboard::isKeyPressed(sf::Keyboard::RControl)) {
-            input_ability = false;
-            if (event.type == sf::Event::KeyReleased) {
-                if (event.key.code == sf::Keyboard::S) {
-                    m_should_display_debug_info = !m_should_display_debug_info;
-                    //m_map->save();
-                    input_ability = true;
-                } else if (event.key.code == sf::Keyboard::L) {
-                    m_should_display_debug_info = !m_should_display_debug_info;
-                    //m_map->load();
-                    input_ability = true;
-                }
-            }
-        } else {
-            input_ability = true;
-        }
+        m_player.input(event);
+        m_menu->input(event);
+    }
+}
 
-        if (input_ability) {
-            m_player.input(event);
-            m_menu->input(event);
-        }
+void App::resize(uint32_t x, uint32_t y) {
+    sf::FloatRect visibleArea(0.f, 0.f, static_cast<float>(x),
+                              static_cast<float>(y));
+    m_window.setView(sf::View(visibleArea));
+    m_window.setSize({x, y});
+    m_menu->update();
+}
+
+void App::process_key(sf::Keyboard::Key code) {
+    switch (code) {
+        case sf::Keyboard::Escape:
+            m_window.setMouseCursorVisible(m_should_fix_cursor);
+            m_should_fix_cursor = !m_should_fix_cursor;
+
+            break;
+        case sf::Keyboard::F3:
+            m_should_display_debug_info = !m_should_display_debug_info;
+            break;
+        default:
+            break;
     }
 }
 
@@ -219,4 +209,8 @@ void App::update() {
 
     handle_events();
     m_player.update(timer.restart().asSeconds());
+}
+
+App::~App() {
+
 }
--- Minecraft/src/App.h
+++ Minecraft/src/App.h
@@ -1,31 +1,31 @@
 #pragma once
 
 #include "pch.h"
-#include "Renderer.h"
 #include "DebugData.h"
 #include "Player.h"
-#include "MapMeshBuilder.h"
-
 
 class Menu;
+class Renderer;
+namespace World {
+    class MapMeshBuilder;
+    class Map;
+}
 
 class App {
 private:
     sf::Text m_text;
     sf::Font m_font;
     DebugData m_debug_data;
-    Menu *m_menu;
-    sf::RenderWindow &m_window;
     Player m_player;
-    Renderer m_renderer;
-    //std::shared_ptr<World::Map> m_map;
+    sf::RenderWindow &m_window;
+
+    std::unique_ptr<Menu> m_menu;
+    std::unique_ptr<Renderer> m_renderer;
     std::shared_ptr<World::Map> m_map;
+    std::unique_ptr<World::MapMeshBuilder> m_map_mesh_builder;
 
     bool m_should_display_debug_info = true;
     bool m_should_fix_cursor = true;
-
-
-    World::MapMeshBuilder m_map_mesh_builder;
 public:
     /* init some objects */
     explicit App(sf::RenderWindow &window);
@@ -33,6 +33,7 @@ public:
     /* Run game cycle */
     void run();
 
+    ~App();
 private:
     /* input and camera */
     void handle_events();
@@ -43,11 +44,17 @@ private:
     /* update entity activity */
     void update();
 
-
     /* send data to render */
     void draw_SFML();
 
     void draw_openGL();
 
+    std::string debug_text();
+
+    void process_key(sf::Keyboard::Key code);
+
+    void resize(uint32_t x, uint32_t y);
+
+    void draw_under_water_filter();
 };
 
